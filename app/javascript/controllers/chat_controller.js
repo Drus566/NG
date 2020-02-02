a86be3fd609e4279929f56312a85f558372cb5ac
Drus566 <ahdpeu566@mail.ru>
2020-02-02 17:08:58 +0500

    work with chat scroll

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -4,42 +4,90 @@ import consumer from "../channels/consumer"
 export default class extends Controller {
 
     static targets = ["messages"]
+
+    initialize() {
+
+    }
     
     connect() {
-        let chat = this
+        this.addActionCable(this)
+    }
+
+    disconnect() {
+        this.subscription.unsubscribe();
+        this.saveScrollPosition(this)
+        console.log('ChatController: Disconnect')
+    }
+
+    addActionCable(chat) {
         this.subscription = consumer.subscriptions.create("ChatChannel", {
                 connected() {
                 },
                 disconnected() {
                 },
                 received(data) {
-                    if (data['chat']) {
-                        chat.element.innerHTML = data['chat']
-                    } else {
-                        if (chat.hasMessagesTarget) {
-                            chat.messagesTarget.insertAdjacentHTML('beforeEnd', data)
-                            if (chat.messagesTarget.scrollTop == chat.messagesTarget.scrollHeight) {
-                                chat.messagesTarget.scrollTop = chat.messagesTarget.scrollHeight
-                            }
-                        }
-                    }
+                    data['chat'] ? chat.addChat(chat, data) : chat.addMessage(chat, data)
                 }
             }
         )
-
-        
     }
 
-    disconnect() {
-        this.subscription.unsubscribe();
-        Console.log('ChatController: Disconnect')
-    }
-
-    addMessage(event) {
-        let [data, status, xhr] = event.detail
-        
-        if (this.hasMessagesTarget) {
-            this.messagesTarget.innerHTML += xhr.response
+    addMessage(chat, data) {
+        if (chat.hasMessagesTarget) {
+            if (chat.messagesTarget.scrollHeight - chat.messagesTarget.scrollTop === chat.messagesTarget.clientHeight) {
+                chat.messagesTarget.insertAdjacentHTML('beforeEnd', data)
+                chat.messagesTarget.scrollTop += chat.messagesTarget.clientHeight
+            } else {
+                chat.messagesTarget.insertAdjacentHTML('beforeEnd', data)
+            }
         }
+    }
+
+    addChat(chat, data) {
+        chat.element.innerHTML = data['chat']
+        chat.addEventOnScroll(chat)
+        chat.setScrollPosition(chat)
+    }
+
+    addEventOnScroll(chat) {
+        let ticking = false
+
+        if (chat.hasMessagesTarget) {
+            chat.messagesTarget.addEventListener('scroll', function(e) {
+                let btn = document.querySelector('.btn-scroll-down')
+                if (!ticking) {
+                    window.requestAnimationFrame(function() {
+                        if (chat.messagesTarget.scrollHeight - chat.messagesTarget.scrollTop !== chat.messagesTarget.clientHeight) {
+                            if (!btn.classList.contains('active')) {
+                                btn.classList.add('active')
+                            }
+                        } else {
+                            if (btn.classList.contains('active')) {
+                                btn.classList.remove('active')
+                            }
+                        }
+                        ticking = false
+                    })
+                    ticking = true
+                }
+            })
+        }
+    }
+
+    scrollToDown() {
+        this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight
+    }
+
+    setScrollPosition(chat) {
+        let scrollPos = localStorage.getItem('scrollPosition')
+
+        if (scrollPos) {
+            chat.messagesTarget.scrollTop = scrollPos
+        }
+    }
+
+    saveScrollPosition(chat) {
+        let scrollPos = chat.messagesTarget.scrollTop
+        localStorage.setItem('scrollPosition', scrollPos)
     }
 }
