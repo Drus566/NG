cc54fa28cf6c87d217c5952e4e992eca0c3d6f9c
Drus566 <ahdpeu566@mail.ru>
2020-02-17 14:58:28 +0500

    upload and remove avatar with input file select and drag and drop

--- app/javascript/controllers/avatar_uploader_controller.js
+++ app/javascript/controllers/avatar_uploader_controller.js
@@ -1,49 +1,97 @@
 import { Controller } from "stimulus"
+import { DirectUpload } from "@rails/activestorage"
 
 export default class extends Controller {
 
     static targets = ['template', 'select']
 
-    processFile(event) {
-        console.log(event)
-        console.log('processFile')
-        let file = event.target.files[0]
-        this.validationFile(file)
+    //onChange
+    selectFile(event) {
+        let files = event.target.files
+        this.handleFiles(files)
     }
 
-    validationFile(file) {
-        if (file && file.type.startsWith('image/')) {
+    handleFiles(files) {
+        ([...files]).forEach(function(file) {
+            this.validateFile(file)
+        }, this)
+    }
+    
+    validateFile(file) {
+        if (file && file.type.startsWith('image/') && file.size <= this.maxFileSize) {
             this.templateTarget.setAttribute('src', URL.createObjectURL(file))
+        } else {
+            this.addError('Недопустимый загружаемый файл')
         }
+    }
+
+    uploadFiles(files) {
+        this.selectTarget.files = files
     }
 
     enterDrag(event) {
         event.stopPropagation();
         event.preventDefault();
-        console.log('dragenter')
     }
 
     overDrag(event) {
         event.stopPropagation();
         event.preventDefault();
-        console.log('dragover')
     }
 
     fileDrop(event) {
         event.stopPropagation();
         event.preventDefault();
-        console.log('drop')
-      
-        let data = event.dataTransfer      
-        this.validationFile(data.files[0])
+        
+        if (typeof(window.FileReader) == 'undefined') {
+            this.addError('Не поддерживается браузером')
+            return false
+        }
+
+        let data = event.dataTransfer
+        let files = data.files      
+
+        this.handleFiles(files)
+        this.uploadFiles(files)
     }
 
-    removeAvatar() {
+    // removeAvatar() {
+    //     event.stopPropagation();
+    //     event.preventDefault();
+
+    //     this.templateTarget.setAttribute('src', '')
+    //     this.selectTarget.setAttribute('value', '')
+        
+    //     let fileBuffer = this.createFileList(this.selectTarget.files)
+    //     console.log(fileBuffer)
+    //     fileBuffer.shift(0, 1)
+    //     console.log(fileBuffer)
+    //     this.selectTarget.files = fileBuffer
+    //     console.log(this.selectTarget.files)
+    // }
+
+    removeAllFiles() {
         event.stopPropagation();
         event.preventDefault();
+
         this.templateTarget.setAttribute('src', '')
         this.selectTarget.setAttribute('value', '')
-        console.log(this.selectTarget.files)
-        this.selectTarget.dispatchEvent(new Event('change'))
+
+        let dataTransfer =  new DataTransfer()
+        this.selectTarget.files = dataTransfer.files
+    }
+
+    // createFileList(files) {
+    //     let fileBuffer = []
+    //     Array.prototype.push.apply(fileBuffer, files)
+    //     return fileBuffer
+    // }
+
+    addError(message) {
+        document.dispatchEvent(new CustomEvent('notify', { detail: `<div class="error">${message}</div>` }))
+    }
+
+    get maxFileSize() {
+        return 10485760 // 10 mB
     }
 }
